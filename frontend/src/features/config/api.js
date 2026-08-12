e64cac8f59d2a693ca199c341c50715d2d2edff0
Edwar Velásquez <ricoprogramar@gmail.com>
2026-08-12 11:31:12 -0500

    fix: API_URL

--- frontend/src/features/config/api.js
+++ frontend/src/features/config/api.js
@@ -1,3 +1,12 @@
+// frontend/src/features/config/api.js
 // Centraliza la URL base de la API para desarrollo y despliegue.
 
-export const API_URL = "http://localhost:4000/api";
+// export const API_URL = "http://localhost:4000/api";
+
+
+//==================================================
+//      CONFIGURACIÓN PARA PRODUCCIÓN        
+//==================================================
+
+// URL base de la API para producción.
+export const API_URL = "http://grupo7.ricoprogramar.com/api";
